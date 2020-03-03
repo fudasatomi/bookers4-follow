3b63e73d58a1c7f8cd6ae0dc4f60303ec39a0dbf
fudasatomi <imotas.aduf@gmail.com>
2020-03-03 20:34:54 +0900

    start

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'home#top'
+  get 'home/about'
+  devise_for :users
+    resources :users,only: [:show,:index,:edit,:update]
+
+  resources :books do
+  	resource :favorites, only:[:create,:destroy]
+  	resource :book_comments, only:[:create, :destroy]
+  end
+
+end
