81d1eedec1c175336c3be12ec1dc9288ffb8819b
fudasatomi <imotas.aduf@gmail.com>
2020-03-05 16:30:55 +0900

    user_show follow-btn

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,15 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   attachment :profile_image, destroy: false
 
-  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :following_relationships, foreign_key: "follower_id",
+                                     class_name: "Relationship",
+                                     dependent: :destroy
 
   has_many :followings, through: :following_relationships
 
-  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :follower_relationships, foreign_key: "following_id",
+                                    class_name: "Relationship",
+                                    dependent: :destroy
 
   has_many :followers, through: :follower_relationships
 
